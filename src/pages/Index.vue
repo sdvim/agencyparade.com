be3a85efef49eea8908e9472eb5b79c3d3eae299
Steve Della Valentina <s.dellavalentina@gmail.com>
2020-06-03 14:36:37 -0600

    Add section to homepage

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -39,6 +39,11 @@
         </div>
       </div>
     </section>
+    <blockquote class="content-center text-center pt9 pb9">
+      <p class="large-text">“Parade functioned as part of our team, helping to crystalize our vision and delivering incredible results in record time”</p>
+      <footer class="small-text">– Brian, Product Manger at Telnyx</footer>
+    </blockquote>
+    <hr>
   </Layout>
 </template>
 
