900868861456be2096fbad479da1b23aa9d7947c
Steve Della Valentina <s.dellavalentina@gmail.com>
2020-07-07 14:19:10 -0600

    Add about page

---
+++ src/pages/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <Layout>
+    <section class="flex flex-column flex-j-center min-height-vh bg-darkgrey color-white pt6 pb6">
+      <div class="content-container flex flex-column">
+        <div class="width-6-col">
+          <h1 class="mb2">What we do</h1>
+        </div>
+        <div class="width-6-col flex-as-end">
+          <p class="ml6c mb1">Parade is a nimble team of strategists and product designers dedicated to creating stunning digital products.</p>
+          <p class="ml6c mb1">Our team has experience designing websites and digital products. Understanding industry and customers shouldn't just inform business strategy, but also each design decision.</p>
+          <p class="ml6c">Using proven design thinking methods, our team works to uncover and blend business goals and user’s needs to build uncompromised digital experiences.</p>
+        </div>
+      </div>
+    </section>
+  </Layout>
+</template>
