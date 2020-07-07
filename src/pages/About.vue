a73563705bcc718325f4cc12d1d3abd1b8651a37
Steve Della Valentina <s.dellavalentina@gmail.com>
2020-07-07 15:57:16 -0600

    Delete unused code

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -16,14 +16,9 @@
 </template>
 
 <script>
-import Author from '~/components/Author.vue'
-import PostCard from '~/components/PostCard.vue'
-
 export default {
-  components: {
-    Author,
-    PostCard
-  },
   metaInfo: {
     title: 'About'
   },
+};
+</script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -90,38 +90,8 @@
   </Layout>
 </template>
 
-<page-query>
-query {
-  posts: allPost(filter: { published: { eq: true }}) {
-    edges {
-      node {
-        id
-        title
-        date (format: "D. MMMM YYYY")
-        timeToRead
-        description
-        cover_image (width: 770, height: 380, blur: 10)
-        path
-        tags {
-          id
-          title
-          path
-        }
-      }
-    }
-  }
-}
-</page-query>
-
 <script>
-import Author from '~/components/Author.vue'
-import PostCard from '~/components/PostCard.vue'
-
 export default {
-  components: {
-    Author,
-    PostCard
-  },
   metaInfo: {
     title: 'Home'
   },
