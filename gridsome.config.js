cdb65f612458795ead47a97db8103418507bf6b5
Steve Della Valentina <s.dellavalentina@gmail.com>
2020-07-11 07:19:18 -0600

    Remove unused files and code

--- gridsome.config.js
+++ gridsome.config.js
@@ -7,39 +7,4 @@
 module.exports = {
   siteName: 'Parade',
   siteDescription: 'Parade is a design team helping businesses elevate their web and mobile experiences.',
-
-  templates: {
-    Post: '/:title',
-    Tag: '/tag/:id'
-  },
-
-  plugins: [
-    {
-      // Create posts from markdown files
-      use: '@gridsome/source-filesystem',
-      options: {
-        typeName: 'Post',
-        path: 'content/posts/*.md',
-        refs: {
-          // Creates a GraphQL collection from 'tags' in front-matter and adds a reference.
-          tags: {
-            typeName: 'Tag',
-            create: true
-          }
-        }
-      }
-    }
-  ],
-
-  transformers: {
-    //Add markdown support to all file-system sources
-    remark: {
-      externalLinksTarget: '_blank',
-      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
-      anchorClassName: 'icon icon-link',
-      plugins: [
-        '@gridsome/remark-prismjs'
-      ]
-    }
-  }
 }
